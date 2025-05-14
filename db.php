ffddc1c98468e4cc25a7273c097670b18fe42076
anynomous <164474776+Philemon2024@users.noreply.github.com>
2025-05-14 16:57:30 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "12GROUP");
+$conn = new mysqli("localhost", "root", "", "blog_system1");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
